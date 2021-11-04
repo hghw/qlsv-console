575c6f03761802f393bf1924394a0ef419233700
Hoàng Gia Huy <89172440+hghw@users.noreply.github.com>
2021-11-04 17:21:31 +0700

    qlsv2
    upfile qlsv 1/3

---
+++ qlnvbt2/qlnvbt2/Models/NhanVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace qlnvbt2.Models
+{
+    class NhanVien
+    {
+        public string maNhanVien { get; set; }
+        public string hoTen { get; set; }
+        public string ngaySinh { get; set; }
+        public string sdt { get; set; }
+        public string diaChi { get; set; }
+        public string chucVu { get; set; }
+        public int soNamCongTac { get; set; }
+    }
+}
